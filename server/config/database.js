716838065ef92365d60343bddf9851a358e20f12
Maxwell Wade <maxwelled3@gmail.com>
2024-07-31 12:09:12 -0500

    Posting Image Completed

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('travel_platform_dev', 'postgres', 'slyfox2708', {
+const sequelize = new Sequelize('travel_platform_dev', 'postgres', 'vraj2003', {
   host: 'localhost',
   dialect: 'postgres', // or 'mysql' or any other supported dialect
   logging: false,
